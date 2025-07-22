0144aaaf51d57d13c6503851d1273b70049074e1
xuanmao <Chenjingxin001@gmail.com>
2025-07-22 09:44:27 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,34 @@
+# =================================================================
+#                 👇 只需在这里修改您的信息 👇
+#                 👇 Just modify your info here 👇
+# =================================================================
+
+# 请在这里填入您的名字或ID
+AUTHOR_NAME = "AIGC玄猫"
+
+# 请在这里填入部署日期
+DEPLOY_DATE = "2025-07-17"
+
+# 您可以在下面的列表中添加、删除或修改任意多行欢迎语
+# 每一行文字用英文双引号 " " 括起来，行与行之间用英文逗号 , 隔开
+WELCOME_LINES = [
+    f"欢迎使用由 {AUTHOR_NAME} 精心配置的 ComfyUI",
+    f"部署日期: {DEPLOY_DATE}",
+    "这是一个稳定、纯净的绿色环境包",
+    "由衷感谢bilibili@秋葉aaaki"
+]
+
+# =================================================================
+#                 👆 修改区域结束，下方代码请勿改动 👆
+#                 👆 End of modification area, do not change the code below 👆
+# =================================================================
+
+
+# --- 自动打印程序，无需修改 ---
+# --- Auto-print program, no modification needed ---
+separator = "=" * 70
+print("\n\n" + separator)
+for line in WELCOME_LINES:
+    # 在每行文字前都增加一个换行符 \n 来创造宽松的间距
+    print(f"\n      {line}")
+print("\n" + separator + "\n\n")
