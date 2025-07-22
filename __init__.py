a6db09873cf8972d0992a122222db85b8efab8a4
Chendounai <Chenjingxin001@gmail.com>
2025-07-22 09:56:58 +0800

    新增公众号信息

--- __init__.py
+++ __init__.py
@@ -14,8 +14,9 @@ DEPLOY_DATE = "2025-07-17"
 WELCOME_LINES = [
     f"欢迎使用由 {AUTHOR_NAME} 精心配置的 ComfyUI",
     f"部署日期: {DEPLOY_DATE}",
-    "这是一个稳定、纯净的绿色环境包",
-    "由衷感谢bilibili@秋葉aaaki"
+    "这是一个稳定、纯净的绿色环境包。",
+    "由衷感谢bilibili@秋葉aaaki",
+    "获取最新版，请关注微信公众号“玄猫AIGC隐研所”发送：安装包"
 ]
 
 # =================================================================
@@ -27,8 +28,14 @@ WELCOME_LINES = [
 # --- 自动打印程序，无需修改 ---
 # --- Auto-print program, no modification needed ---
 separator = "=" * 70
-print("\n\n" + separator)
+print(f"\n\n{separator}")
 for line in WELCOME_LINES:
     # 在每行文字前都增加一个换行符 \n 来创造宽松的间距
     print(f"\n      {line}")
 print("\n" + separator + "\n\n")
+
+
+# This tells ComfyUI that this custom node does not add any new nodes.
+# It's just for running the code above on startup.
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
