c4c95d20775e6dbc34e87b4e66a3082810b3c067
Chendounai <Chenjingxin001@gmail.com>
2025-08-04 11:07:31 +0800

    更新了数字人和其他视频插件，需要+V联系

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@ DEPLOY_DATE = "2025-08-04"
 WELCOME_LINES = [
     f"欢迎使用由 {AUTHOR_NAME} 精心配置的 ComfyUI",
     f"部署日期: {DEPLOY_DATE}",
+    "更新了数字人和其他视频插件，需要↓领取",
     "这是一个稳定、纯净的绿色环境包。",
     "由衷感谢bilibili@秋葉aaaki",
     "获取最新版，请关注微信公众号“玄猫AIGC隐研所”发送：安装包"
