792fc9647464d237fb2eaa96000479de9a07f362
Chendounai <Chenjingxin001@gmail.com>
2025-09-01 10:24:43 +0800

    更新nunchaku1.0.0的轮子依赖

--- __init__.py
+++ __init__.py
@@ -7,14 +7,14 @@
 AUTHOR_NAME = "AIGC玄猫"
 
 # 请在这里填入部署日期
-DEPLOY_DATE = "2025-08-22"
+DEPLOY_DATE = "2025-09-01"
 
 # 您可以在下面的列表中添加、删除或修改任意多行欢迎语
 # 每一行文字用英文双引号 " " 括起来，行与行之间用英文逗号 , 隔开
 WELCOME_LINES = [
     f"欢迎使用由 {AUTHOR_NAME} 精心配置的 ComfyUI",
     f"部署日期: {DEPLOY_DATE}",
-    "更新了diffusers库，可以更好使用Qwen编辑模型",
+    "更新了nunchaku1.0.0依赖，可以使用Qwen了",
     "这是一个稳定、纯净的绿色环境包。",
     "由衷感谢bilibili@秋葉aaaki",
     "获取最新版，请关注微信公众号“玄猫AIGC隐研所”发送：安装包"
